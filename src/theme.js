11cbd59d01cf5915c2e145d339006b3a840bf22e
toukirislam <shovoodev@gmail.com>
2024-11-05 09:47:13 +0100

    initilization first

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+        background: {default : '#1d1f26',
+            
+        }
+    }
+})
+
+export default theme
